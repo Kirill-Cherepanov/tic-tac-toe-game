c3d3adc26d1af472ebe51a78ea611ddfa6b71c8a
Cherepanov Kirill <cherkirr@gmail.com>
2022-03-26 21:49:31 +0300

    refactor: prettify .ts files

--- scripts/TicTacToeGame.ts
+++ scripts/TicTacToeGame.ts
@@ -9,7 +9,16 @@ export default class TicTacToeGame {
     xTurn: boolean;
     WINNING_COMBINATIONS: [number, number, number][];
 
-    constructor(cells: NodeListOf<HTMLDivElement>, board: HTMLDivElement, endMessage: HTMLDivElement, endText: HTMLDivElement, restartButton: HTMLDivElement, X_CLASS: string, O_CLASS: string, xTurn: boolean = true) {
+    constructor(
+        cells: NodeListOf<HTMLDivElement>,
+        board: HTMLDivElement,
+        endMessage: HTMLDivElement,
+        endText: HTMLDivElement,
+        restartButton: HTMLDivElement,
+        X_CLASS: string,
+        O_CLASS: string,
+        xTurn: boolean = true
+    ) {
         this.cells = cells;
         this.board = board;
         this.endMessage = endMessage;
@@ -29,16 +38,16 @@ export default class TicTacToeGame {
             [6, 7, 8],
             [0, 4, 8],
             [2, 4, 6]
-        ]
+        ];
     }
 
     startGame(): void {
         this.setBoardHoverClass();
         this.endMessage.classList.remove('show');
-        this.cells.forEach(cell => {
+        this.cells.forEach((cell) => {
             cell.classList.remove(this.X_CLASS);
             cell.classList.remove(this.O_CLASS);
-            cell.addEventListener('click', this.makeMove, {once: true});
+            cell.addEventListener('click', this.makeMove, { once: true });
         });
         this.restartButton.addEventListener('click', this.startGame.bind(this));
     }
@@ -72,22 +81,26 @@ export default class TicTacToeGame {
     }
 
     protected checkWin(currentClass: string): boolean {
-        return this.WINNING_COMBINATIONS.some(combination => {
-            return combination.every(index => {
+        return this.WINNING_COMBINATIONS.some((combination) => {
+            return combination.every((index) => {
                 return this.cells[index].classList.contains(currentClass);
             });
         });
     }
 
     protected checkDraw(): boolean {
-        return [...this.cells].every(cell => {
-            return cell.classList.contains(this.X_CLASS) || 
-                cell.classList.contains(this.O_CLASS);
+        return [...this.cells].every((cell) => {
+            return (
+                cell.classList.contains(this.X_CLASS) ||
+                cell.classList.contains(this.O_CLASS)
+            );
         });
     }
 
     protected gameOver(draw: boolean = false): void {
-        this.endText.innerText = draw ? 'Draw!' : `${this.xTurn ? 'Circle' : 'Cross'} player Wins!`;
+        this.endText.innerText = draw
+            ? 'Draw!'
+            : `${this.xTurn ? 'Circle' : 'Cross'} player Wins!`;
         this.endMessage.classList.add('show');
     }
 }
--- scripts/index.ts
+++ scripts/index.ts
@@ -3,12 +3,23 @@ import TicTacToeGame from '../scripts/TicTacToeGame.js';
 const X_CLASS: string = 'x';
 const O_CLASS: string = 'o';
 
-const cellElements: NodeListOf<HTMLDivElement> = document.querySelectorAll('[data-cell]');
+const cellElements: NodeListOf<HTMLDivElement> =
+    document.querySelectorAll('[data-cell]');
 const board = <HTMLDivElement>document.querySelector('.gameboard');
 const endMessage = <HTMLDivElement>document.querySelector('.end-message');
 const endText = <HTMLDivElement>document.querySelector('[data-end-text]');
-const restartButton = <HTMLDivElement>document.querySelector('[data-restart-button]');
+const restartButton = <HTMLDivElement>(
+    document.querySelector('[data-restart-button]')
+);
 
-const ticTacToe: TicTacToeGame = new TicTacToeGame(cellElements, board, endMessage, endText, restartButton, X_CLASS, O_CLASS);
+const ticTacToe: TicTacToeGame = new TicTacToeGame(
+    cellElements,
+    board,
+    endMessage,
+    endText,
+    restartButton,
+    X_CLASS,
+    O_CLASS
+);
 
 ticTacToe.startGame();
